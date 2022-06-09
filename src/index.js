ba113f3b1d4b3ea781a8f478fb56066f433b4549
abdul wahab <abdul.wahab394.aw@gmail.com>
2022-06-09 20:48:20 +0500

    table icons issue fixed

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { store, persistor } from "./redux/store";
 
 
 import "./index.css";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
